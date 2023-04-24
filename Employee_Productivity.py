ad57964a49e6a8769762316803bcd671f5a4cdfe
Aman Pandey <69633924+Amanpandey046@users.noreply.github.com>
2023-04-24 22:57:09 +0530

    Python File For Forecast Employee Productivity
    Last Check in 22:56 24/05/2023

---
+++ Employee_Productivity.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+#importing the necessary libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+# In[40]:
+
+
+import warnings
+warnings.filterwarnings('ignore')
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# ### Data
+
+# #### Importing the Data
+
+# In[7]:
+
+
+# Importing the csv file
+data = pd.read_excel('Employee_Performance_Data.xls')
+
+
+# #### Exploratory Data Analysis
+
+# In[8]:
+
+
+data.shape
+
+
+# In[9]:
+
+
+data.columns
+
+
+# In[11]:
+
+
+data.head(5)
+
+
+# In[12]:
+
+
+#Looking for missing Data
+data.info()
+
+
+# In[13]:
+
+
+data.isnull().sum().sum()
+
+
+# ## Analysis of Department wise Perfomance
+
+# In[14]:
+
+
+# A new pandas Dataframe is created to analyze department wise performance as asked.
+dept = data.iloc[:,[5,27]].copy()
+dept_per = dept.copy()
+
+
+# In[15]:
+
+
+# Finding out the mean performance of all the departments and plotting its bar graph using seaborn.
+dept_per.groupby(by='EmpDepartment')['PerformanceRating'].mean()
+
+
+# In[16]:
+
+
+plt.figure(figsize=(10,4.5))
+sns.barplot(dept_per['EmpDepartment'],dept_per['PerformanceRating'])
+
+
+# In[17]:
+
+
+# Analyze each department separately
+dept_per.groupby(by='EmpDepartment')['PerformanceRating'].value_counts()
+
+
+# In[18]:
+
+
+# Creating a new dataframe to analyze each department separately
+department = pd.get_dummies(dept_per['EmpDepartment'])
+performance = pd.DataFrame(dept_per['PerformanceRating'])
+dept_rating = pd.concat([department,performance],axis=1)
+
+
+# In[19]:
+
+
+# Plotting a separate bar graph for performance of each department using seaborn
+plt.figure(figsize=(15,10))
+plt.subplot(2,3,1)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Data Analysis'])
+plt.subplot(2,3,2)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Sales'])
+plt.subplot(2,3,3)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Software Engineer'])
+plt.subplot(2,3,4)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Human Resources'])
+plt.subplot(2,3,5)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Finance'])
+plt.subplot(2,3,6)
+sns.barplot(dept_rating['PerformanceRating'],dept_rating['Data Science'])
+plt.show()
+
+
+# ## Data Processing/ Data Munging
+# 
+
+# In[20]:
+
+
+# Encoding all the ordinal columns and creating a dummy variable for them to see if there are any effects on Performance Rating
+enc = LabelEncoder()
+for i in (2,3,4,5,6,7,16,26):
+    data.iloc[:,i] = enc.fit_transform(data.iloc[:,i])
+data.head()
+
+
+# ## Feature Selection
+
+# ##### There are a lot of columns in the predictor variable. So, the correlation coeffecient is calculated to see which of them are important and these are then used for training methods. From there, we also get the top factors which affect performance. We can see that the most important features selectd were Department, Job Role, Environment Satisfaction, Last Salary Hike Percent, Work Life Balance, Experience Years At This Company, Experience Years In Current Role, Years Since Last Promotion, Years With Current Manager. These were selected because their correlation coeffecient with Performance Rating was more than 0.1. 
+# 
+# ##### Standardization and Label Encoding was also used for feature transformation.
+# 
+# ##### A separate analysis considering all the predictors was carried out but it resulted in decreasing the accuracy. Similarly, 
+# 
+# ##### Principal Component Analysis also reduces the accuracy.
+# 
+# ##### Top 3 factors which affect the employee performance are 1. Employee EnvironmentSatisfaction, 2. Employee Last Salary Hike Percent and 3. Years Since Last Promotion
+
+# In[21]:
+
+
+# Finding out the correlation coeffecient to find out which predictors are significant.
+data.corr()
+
+
+# In[22]:
+
+
+# Dropping the first columns as it is of no use for analysis.
+data.drop(['EmpNumber'],inplace=True,axis=1)
+
+
+# In[23]:
+
+
+data.head(5)
+
+
+# In[25]:
+
+
+# Here we have selected only the important columns
+y = data.PerformanceRating
+#X = data.iloc[:,0:-1]  All predictors were selected it resulted in dropping of accuracy.
+X = data.iloc[:,[4,5,9,16,20,21,22,23,24]] # Taking only variables with correlation coeffecient greater than 0.1
+X.head()
+
+
+# In[26]:
+
+
+# Splitting into train and test for calculating the accuracy
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=10)
+
+
+# In[31]:
+
+
+# Standardization technique is used
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+# In[32]:
+
+
+X_train.shape
+
+
+# In[34]:
+
+
+X_test.shape
+
+
+# ## Training the model
+
+# In[36]:
+
+
+from sklearn.linear_model import LogisticRegression
+model_logr = LogisticRegression()
+model_logr.fit(X_train,y_train)
+
+
+# In[37]:
+
+
+# Predicting the model
+y_predict_log = model_logr.predict(X_test)
+
+
+# In[38]:
+
+
+# Finding accuracy, precision, recall and confusion matrix
+print(accuracy_score(y_test,y_predict_log))
+print(classification_report(y_test,y_predict_log))
+
